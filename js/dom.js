e626757917a250775ba0277a7fc59f2687981d9e
rayyan2300 <rayyan3379@gmail.com>
2023-08-21 19:09:14 +0600

    added total payment

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,5 @@
+let total = 0;
+
 function cardClick(target) {
   const selectedItems = document.getElementById("selected");
   //   console.log(target.parentNode.childNodes[1]);
@@ -6,8 +8,28 @@ function cardClick(target) {
 
   const li = document.createElement("li");
   li.innerText = itemName;
-  console.log(li);
+
   selectedItems.appendChild(li);
+
+  // const totalPrice = document.getElementById("total");
+  // console.log(totalPrice);
+  const priceSpan = target.querySelector(".card-actions span").innerText;
+
+  total = parseFloat(total) + parseFloat(priceSpan);
+  document.getElementById("total").innerText = total;
+
+  document.getElementById("applyBtn").addEventListener("click", function () {
+    const couponField = document.getElementById("couponId").value;
+    // console.log(couponField);
+
+    if (couponField === "SELL200") {
+      discount = total * 0.2;
+      totalPrice = total - discount;
+      console.log(totalPrice);
+    } else {
+      console.log(20);
+    }
+  });
 }
 
 // const itemName =
